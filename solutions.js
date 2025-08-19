d7bd813096f617d6e07f4f6f5d59d9f53161b407
Katia-Alcocer <ki.alcoceraguilar@ugto.mx>
2025-08-18 20:16:53 -0600

    I finished the functions

--- solutions.js
+++ solutions.js
@@ -1,25 +1,63 @@
 // Sum of Two Numbers
 function sum(a, b) {
+  return a + b;
   // Add your code here
 }
 
 // Factorial of a Number
 function factorial(n) {
+  factorial = 1;
+
+  for (let i =1; i <= n; i++) {
+    factorial=factorial*i;
+  }
+
+  return factorial;
   // Add your code here
 }
 
 // Find the Largest Number
 function findLargest(arr) {
+  let largest = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+      if (arr[i] > largest) {
+        largest = arr[i];
+      }
+    }
+  
+    return largest;
+
   // Add your code here
 }
 
 // Count Vowels in a String
 function countVowels(str) {
+  let count = 0;
+  const vowels = "aeiou";
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      count++;
+    }
+  }
+  return count;
   // Add your code here
 }
 
 // Check if a Number is Prime
 function isPrime(n) {
+
+  if (n <= 1) return false;  
+  
+  if (n === 2 || n === 3) return true;
+  if (n % 2 === 0) return false;  
+  if (n % 3 === 0) return false;  
+
+ //square root
+  for (let i = 5; i * i <= n; i += 2) {
+    if (n % i === 0) return false;
+  }
+  return true;
   // Add your code here
 }
 
