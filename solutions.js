c59a98e923f3bf518b2cdb4e79dae593a360546e
Katia-Alcocer <ki.alcoceraguilar@ugto.mx>
2025-08-18 20:41:17 -0600

    I added mayusc vowelst

--- solutions.js
+++ solutions.js
@@ -53,7 +53,7 @@ function countVowels(str) {
     }
 
   let count = 0;
-  const vowels = "aeiou";
+  const vowels = "aeiouAEIOU";
   for (let i = 0; i < str.length; i++) {
     if (vowels.includes(str[i])) {
       count++;
